9e3f59aad3b4761a26b7327bb1a80145d6d8780e
alext <alext@pissedconsumer.com>
2017-02-28 09:10:17 +0300

    changes to amp styles v2

--- content/themes/custom-theme/dev/js/small-config.js
+++ content/themes/custom-theme/dev/js/small-config.js
@@ -1,5 +1,5 @@
 WebFont.load({
     google: {
-        families: ['Open+Sans:400,700:cyrillic', 'Roboto:400,700:cyrillic']
+        families: ['Open+Sans:400,700:cyrillic', 'Merriweather:400,700:cyrillic']
     }
 });
